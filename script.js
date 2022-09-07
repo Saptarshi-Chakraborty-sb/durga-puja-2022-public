721cfe5d0b5be7f073fd0bb61017942d22f5b571
Saptarshi Chakraborty <saptarshichakraborty04@outlook.com>
2022-09-08 01:44:18 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,11 @@
+
+let table = document.getElementById('content-table');
+let tr = document.createElement('tr');
+
+document.addEventListener("load", appendAllImageData);
+
+
+
+function appendAllImageData() {
+    console.log(table);
+}
